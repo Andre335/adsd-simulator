8ca842d5b230d743b95b7463b71cdfe71a0962d1
Andre Goncalves <andre335@gmail.com>
2018-06-29 00:58:20 -0300

    Baggage Claim

--- airport.py
+++ airport.py
@@ -8,6 +8,8 @@ NUM_SECURITY = 7
 NUM_GATES = 3
 NUM_IMMIGRATIONS = 6
 NUM_CUSTOMS = 4
+NUM_LUGGAGE = 2
+REPAIR_EXTRAVIATION_PROBLEM = 27.0			# Time it takes to repair a extraviation in minutes
 REPAIR_CUSTOM_PROBLEM = 18.0			# Time it takes to repair a custom in minutes
 REPAIR_PLANE_PROBLEM = 230.0			# Time it takes to repair a plane in minutes
 REPAIR_SECURITY_PROBLEM = 100.0			# Time it takes to repair a security checking in minutes
@@ -19,6 +21,43 @@ MTTF = 300.0							# Mean Attendant time to failure in minutes
 MEAN = 1 / MTTF							# Repair time
 WEEKS = 4								# Time the simulation took
 SIM_TIME = WEEKS * 7 * 24 * 60  		# Simulation time in Minutes
+
+def time_to_luggage_extraviation():
+	return random.expovariate(MEAN)
+
+def passenger_luggage_time():
+	return random.normalvariate(PT_MEAN, PT_SIGMA)
+	
+class Baggage(object):
+	def __init__(self, env, name):
+		self.env = env
+		self.name = name
+		self.extraviated = False
+		self.passengers_passed = 0
+	
+		self.process = env.process(self.running())
+		env.process(self.extraviate())
+	
+	def running(self):
+		while True:
+			done_in = passenger_luggage_time()
+			while done_in:
+				try:
+					start = self.env.now
+					yield self.env.timeout(done_in)
+					done_in = 0
+				except simpy.Interrupt:
+					self.extraviated = True
+					done_in -= self.env.now - start
+					yield self.env.timeout(REPAIR_EXTRAVIATION_PROBLEM)
+					self.extraviated = False
+			self.passengers_passed += 1
+	
+	def extraviate(self):
+		while True:
+			yield self.env.timeout(time_to_luggage_extraviation())
+			if not self.extraviated:
+				self.process.interrupt()
 
 def time_to_immigration_failure():
 	return random.expovariate(MEAN)
@@ -246,42 +285,56 @@ class Machine(object):
 				self.process.interrupt()
                 
 # Setting Up and Starting Simulation
-print('Airport')
+print('Aeroporto')
 random.seed(RANDOM_SEED)
 
 # Create an environment and start the setup process
 env = simpy.Environment()
 
-machines = [Machine(env, 'Auto Atendimento %d' % (i+1))
-            for i in range(NUM_AUTO_MACHINES)]
-attendants = [Atendente(env, 'Atendente %d' % (i+1))
-			for i in range(NUM_ATENDENTES)]
-securities = [Security(env, 'Checagem de Seguranca %d' % (i+1))
-			for i in range(NUM_SECURITY)]
+machines = [Machine(env, 'Auto Atendimento %d' % (i+1)) for i in range(NUM_AUTO_MACHINES)]
+attendants = [Atendente(env, 'Atendente %d' % (i+1)) for i in range(NUM_ATENDENTES)]
+securities = [Security(env, 'Checagem de Seguranca %d' % (i+1)) for i in range(NUM_SECURITY)]
 planes = [Plane(env, 'Aviao %d' % (i+1)) for i in range(NUM_GATES)]
 immigrations = [Immigration(env, 'Imigracao %d' % (i+1)) for i in range(NUM_IMMIGRATIONS)]
 customs = [Custom(env, 'Anfandega %d' % (i+1)) for i in range(NUM_CUSTOMS)]
+baggages = [Baggage(env, 'Retirada de Bagagem %d' % (i+1)) for i in range(NUM_LUGGAGE)]
 
 # Executing
 env.run(until=SIM_TIME)
 
 # Results
-print('Airport results after %d weeks' % WEEKS)
+print('Resultados do aeroporto depois de %d semanas\n' % WEEKS)
+print('Auto Atendimentos: ')
 for machine in machines:
     print('%s atendeu %d clientes.' % (machine.name, machine.clients_attended))
+print
 
+print('Atendentes: ')
 for attendant in attendants:
     print('%s atendeu %d clientes.' % (attendant.name, attendant.clients_attended))
+print
 
+print('Checagens de seguranca: ')
 for security in securities:
-    print('%s checou %d passageiros.' % (security.name, security.passengers_checked))
+    print('%s revistou %d passageiros.' % (security.name, security.passengers_checked))
+print
 
+print('Avioes: ')
 for plane in planes:
     print('%d passageiros embarcaram no %s.' % (plane.passengers_boarded, plane.name))
-    
+print
+
+print('Imigracao: ')
 for immigration in immigrations:
     print('%d passageiros passaram pela %s.' % (immigration.passengers_immigrated, immigration.name))
+print
 
+print('Alfandega: ')
 for custom in customs:
     print('%d passageiros passaram pela %s.' % (custom.passengers_analized, custom.name))
+print
+
+print('Recepcao de Bagagem: ')
+for bagagge in baggages:
+    print('%d passageiros pegaram malas no %s.' % (bagagge.passengers_passed, bagagge.name))
 #print('%s had %d passengers this month.' % (airport.name, airport.passengers_travelled))
